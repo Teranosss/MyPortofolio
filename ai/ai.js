48ca265b789c077d3e14d0d33149a7f40a149f4c
Amarsya Terano <aysraterano@gmail.com>
2026-06-18 01:43:53 +0700

    Update ai.js

--- ai/ai.js
+++ ai/ai.js
@@ -1,6 +1,6 @@
 // ── BACKEND URL ──
 
-const BACKEND_URL = 'https://teranosss-beckendaiweb.hf.space/chat';
+const BACKEND_URL = 'https://teranosss-beckendaiweb.hf.space/';
 
 const messagesEl  = document.getElementById('chatMessages');
 const welcomeEl   = document.getElementById('welcomeState');
